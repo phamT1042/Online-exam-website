07b7f3447b5e610164af0899f6c4a5c4273d64a8
VanTienDesktop <pvantien203@gmail.com>
2024-04-12 17:35:11 +0700

    change folder

--- FE/web/src/app/(student)/layout.js
+++ FE/web/src/app/(student)/layout.js
@@ -1,19 +1,6 @@
 'use client'
 
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export default function Layout({ children }) {
-    const router = useRouter();
-
-    useEffect(() => {
-        const token = sessionStorage.getItem('token');
-
-        if (token && token !== null) {
-            router.replace('/dashboard');
-        }
-    }, []);
-
     return (
         <>
             {children}
